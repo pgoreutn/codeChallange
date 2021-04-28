d152771c7fe7368c9459b247e5cc1e19df8bffc5
Pablo Martin Gore <pgore@innovabc.com.ar>
2021-04-28 13:46:35 -0300

    add mysql docker container , init product table and configure spring boot application

---
+++ src/main/java/com/pablogore/code/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.pablogore.code.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST Controller for all product functionality i. 
+ * This includes searching entities,
+ * creating entities, updating entities, etc.
+ */
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+}
